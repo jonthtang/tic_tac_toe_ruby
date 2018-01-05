d1493993474a01c7e4b7e8f7d7caa92fec51e3ee
jonthtang <jonthtang@gmail.com>
2018-01-05 14:17:22 +0800

    PvC mode added

--- ttt.rb
+++ ttt.rb
@@ -25,6 +25,16 @@ def player_turn_message(board)
   end
 end
 
+def computer_turn_message(board)
+  num = turn_count(board)
+  if num%2!=0
+    puts "It's player one's (O) turn! Please make a move!"
+  else
+    puts "It's the computer's (X) turn! Please wait while it thinks!"
+    sleep 1
+  end
+end
+
 def convert_to_index (input)
   input.to_i-1
 end
@@ -68,16 +78,20 @@ def win_status(arrays_in_array)
 end
 
 def play_again
-  puts "Play again? Type 'yes' or 'y' to play again. Any other key will exit the system."
+  puts "Play again?\nType 'yes' or 'y' to play again.\nType 'no', 'n' or 'exit' to stop the program."
   input = gets.chomp.downcase
     if input == "yes"|| input == "y"
       game_on
-    else
+    elsif input == "no"|| input == "n"|| input == "exit"
       puts "Thanks for playing!"
+    else
+      system "clear"
+      puts "I didn't understand that. Please try again."
+      play_again
     end
 end
 
-def game_on
+def pvp_game
   board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   puts "Let's play a game. Please choose a number between 1-9. You may stop the game at any time by typing 'exit'!"
   loop do
@@ -109,6 +123,76 @@ def game_on
       play_again
       break
     end
+  end
+end
+
+def pvc_game
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  puts "Let's play a game. You are player '1'. Please choose a number between 1-9. You may stop the game at any time by typing 'exit'!"
+  loop do
+    if turn_count(board).odd?
+      display_board(board)
+      player_turn_message(board)
+      input = gets.chomp
+      if input == "exit"
+        puts "Bye! Thanks for playing!"
+        break
+      else
+        ref_array=["1","2","3","4","5","6","7","8","9"]
+        if ref_array.include?(input)
+          system "clear"
+          move_update(board,input)
+          if turn_count(board) > board.size
+            puts "It's a draw! What a shame..."
+            display_board(board)
+            play_again
+            break
+          end
+        else
+          system "clear"
+          puts "I'm sorry, your input in invalid. Please choose a number between 1-9."
+        end
+      end
+      check_win = board_splits_for_check(board)
+      if win_status(check_win) == true
+        display_board(board)
+        play_again
+        break
+      end
+    else
+      display_board(board)
+      computer_turn_message(board)
+      system "clear"
+      available= board.select {|num| num.is_a? Integer}
+      comp_move = available.sample
+      move_update(board,comp_move)
+      if turn_count(board) > board.size
+        puts "It's a draw! What a shame..."
+        display_board(board)
+        play_again
+        break
+      end
+      check_win = board_splits_for_check(board)
+      if win_status(check_win) == true
+        display_board(board)
+        play_again
+        break
+      end
+    end
+  end
+end
+
+def game_on
+  puts "Start a new game.\nPlease select '1' for player vs player\nPlease select '2' for player vs computer."
+  input = gets.chomp
+  if input == "1"
+    pvp_game
+  elsif input == "2"
+    pvc_game
+  else
+    system "clear"
+    puts "I didn't understand that. Please try again."
+    game_on
   end
 end
 
